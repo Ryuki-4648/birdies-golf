ad921049830aa5943fb8010ec0a2e5e6e710bd45
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2023-06-26 22:39:30 +0900

    カウントアップとカウントダウン機能

--- birdies/src/App.tsx
+++ birdies/src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [parCount, setParCount] = useState(1);
+  const [scoreCount, setScoreCount] = useState(1);
+  if (scoreCount === 1) {
+    console.log("ホールインワン");
+  } else if (parCount - 3 === scoreCount) {
+    console.log("アルバトロス");
+  } else if (parCount - 2 === scoreCount) {
+    console.log("イーグル");
+  } else if (parCount - 1 === scoreCount) {
+    console.log("バーディー");
+  } else if (parCount === scoreCount) {
+    console.log("パー");
+  } else if (parCount + 1 === scoreCount) {
+    console.log("ボギー");
+  } else if (parCount + 2 === scoreCount) {
+    console.log("ダブルボギー");
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
       </header>
+      <div className="">
+        <div className="flex">
+          <button className="text-md" onClick={() => setParCount(parCount - 1)}>
+            −
+          </button>
+          <p>{parCount}</p>
+          <button className="text-md" onClick={() => setParCount(parCount + 1)}>
+            ＋
+          </button>
+        </div>
+        <div className="flex">
+          <button
+            className="text-md"
+            onClick={() => setScoreCount(scoreCount - 1)}
+          >
+            −
+          </button>
+          <p>{scoreCount}</p>
+          <button
+            className="text-md"
+            onClick={() => setScoreCount(scoreCount + 1)}
+          >
+            ＋
+          </button>
+        </div>
+        <p className="hoge"></p>
+        <button className="finish">Finish</button>
+      </div>
     </div>
   );
 }
