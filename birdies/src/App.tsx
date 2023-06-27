36135a4dbee9a008d1d685c99d40df060448f13d
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2023-06-27 19:09:49 +0900

    1-18ホール分のtrをループさせる

--- birdies/src/App.tsx
+++ birdies/src/App.tsx
@@ -5,53 +5,94 @@ import "./App.css";
 function App() {
   const [parCount, setParCount] = useState(1);
   const [scoreCount, setScoreCount] = useState(1);
-  if (scoreCount === 1) {
-    console.log("ホールインワン");
-  } else if (parCount - 3 === scoreCount) {
-    console.log("アルバトロス");
-  } else if (parCount - 2 === scoreCount) {
-    console.log("イーグル");
-  } else if (parCount - 1 === scoreCount) {
-    console.log("バーディー");
-  } else if (parCount === scoreCount) {
-    console.log("パー");
-  } else if (parCount + 1 === scoreCount) {
-    console.log("ボギー");
-  } else if (parCount + 2 === scoreCount) {
-    console.log("ダブルボギー");
-  }
+  const [displayResultText, setDisplayResltText] = useState("");
+  const onClickFinishButton = () => {
+    if (scoreCount === 1) {
+      setDisplayResltText("ホールインワン");
+    } else if (parCount - 4 === scoreCount) {
+      setDisplayResltText("コンドル");
+    } else if (parCount - 3 === scoreCount) {
+      setDisplayResltText("アルバトロス");
+    } else if (parCount - 2 === scoreCount) {
+      setDisplayResltText("イーグル");
+    } else if (parCount - 1 === scoreCount) {
+      setDisplayResltText("バーディー");
+    } else if (parCount === scoreCount) {
+      setDisplayResltText("パー");
+    } else if (parCount + 1 === scoreCount) {
+      setDisplayResltText("ボギー");
+    } else if (parCount + 2 === scoreCount) {
+      setDisplayResltText("ダブルボギー");
+    }
+  };
+  const createTableRows = () => {
+    const tableRows = [];
+    for (let i = 1; i <= 18; i++) {
+      tableRows.push(
+        <tr>
+          <td>{i}</td>
+          <td>
+            <div className="flex">
+              <button
+                className="text-md"
+                onClick={() => setParCount(parCount - 1)}
+              >
+                −
+              </button>
+              <p>{parCount}</p>
+              <button
+                className="text-md"
+                onClick={() => setParCount(parCount + 1)}
+              >
+                ＋
+              </button>
+            </div>
+          </td>
+          <td>
+            <div className="flex">
+              <button
+                className="text-md"
+                onClick={() => setScoreCount(scoreCount - 1)}
+              >
+                −
+              </button>
+              <p>{scoreCount}</p>
+              <button
+                className="text-md"
+                onClick={() => setScoreCount(scoreCount + 1)}
+              >
+                ＋
+              </button>
+            </div>
+          </td>
+          <td>
+            <p className="hoge">{displayResultText}</p>
+          </td>
+        </tr>
+      );
+    }
+    return tableRows;
+  };
   return (
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
       </header>
       <div className="">
-        <div className="flex">
-          <button className="text-md" onClick={() => setParCount(parCount - 1)}>
-            −
-          </button>
-          <p>{parCount}</p>
-          <button className="text-md" onClick={() => setParCount(parCount + 1)}>
-            ＋
-          </button>
-        </div>
-        <div className="flex">
-          <button
-            className="text-md"
-            onClick={() => setScoreCount(scoreCount - 1)}
-          >
-            −
-          </button>
-          <p>{scoreCount}</p>
-          <button
-            className="text-md"
-            onClick={() => setScoreCount(scoreCount + 1)}
-          >
-            ＋
-          </button>
-        </div>
-        <p className="hoge"></p>
-        <button className="finish">Finish</button>
+        <table>
+          <thead>
+            <tr>
+              <th>ホール</th>
+              <th>パー</th>
+              <th>スコア</th>
+              <th>コメント</th>
+            </tr>
+          </thead>
+          <tbody>{createTableRows()}</tbody>
+        </table>
+        <button className="finish" onClick={onClickFinishButton}>
+          Finish
+        </button>
       </div>
     </div>
   );
