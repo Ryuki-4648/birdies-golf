51894bd6609655196da7bdd7a7fe585c95a75bc7
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2023-07-17 21:54:05 +0900

    パートスコアの差などを表示させる

--- birdies/src/App.tsx
+++ birdies/src/App.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import "./App.css";
+import { Header } from "./components/header";
 
 function App() {
   const [parCounts, setParCounts] = useState(Array(19).fill(1)); // 1-18ホール分のパーを格納する配列。初期値を1とする。
   const [scoreCounts, setScoreCounts] = useState(Array(19).fill(1)); // Array.prototype.fill() 開始位置〜終了位置（までのすべての要素を、静的な値に変更した配列を返す
 
-  const [displayResultTexts, setDisplayResultTexts] = useState(
-    Array(19).fill("")
-  );
+  // const [displayResultTexts, setDisplayResultTexts] = useState(
+  //   Array(19).fill("")
+  // );
 
   const [resultTotalScore, setResultTotalScore] = useState(0);
 
@@ -15,10 +16,35 @@ function App() {
     const sum = scoreCounts.reduce(
       (accumulator, currentValue) => accumulator + currentValue // accumulator: 現在の合計値 currentValue: 現在の要素の値
     );
-    console.log(sum - 1);
-    console.log(scoreCounts);
-    console.log(parCounts);
+    // console.log(sum - 1);
+    // console.log(scoreCounts);
+    // console.log(parCounts);
     setResultTotalScore(sum);
+  };
+
+  const calculateScoreDifference = (index: number) => {
+    return scoreCounts[index] - parCounts[index];
+  };
+
+  const displayResultText = (index: number) => {
+    const difference = scoreCounts[index] - parCounts[index];
+    if (difference === 0) {
+      return "ホールインワン";
+    } else if (difference === -4) {
+      return "コンドル";
+    } else if (difference === -3) {
+      return "アルバトロス";
+    } else if (difference === -2) {
+      return "イーグル";
+    } else if (difference === -1) {
+      return "バーディー";
+    } else if (difference === 1) {
+      return "ボギー";
+    } else if (difference === 2) {
+      return "ダブルボギー";
+    } else {
+      return;
+    }
   };
 
   const onClickResetButton = () => {
@@ -41,77 +67,20 @@ function App() {
     }
   };
 
-  // const onClickFinishButton = (
-  //   index: number,
-  //   event: React.MouseEvent<HTMLButtonElement>
-  // ) => {
-  //   const scoreCount = scoreCounts[index];
-  //   const parCount = parCounts[index];
-  //   console.log(scoreCount); // onClickFinishButton(hoge, event)としたら、hoge番目のスコアが表示される
-  //   console.log(parCount);
-  //   if (scoreCount === 1) {
-  //     setDisplayResultTexts((prevResultTexts) => {
-  //       console.log(prevResultTexts); // 配列0-18
-  //       const newResultTexts = [...prevResultTexts];
-  //       newResultTexts[index] = "ホールインワン";
-  //       return newResultTexts;
-  //     });
-  //   } else if (parCount - 4 === scoreCount) {
-  //     setDisplayResultTexts((prevResultTexts) => {
-  //       const newResultTexts = [...prevResultTexts];
-  //       newResultTexts[index] = "コンドル";
-  //       return newResultTexts;
-  //     });
-  //   } else if (parCount - 3 === scoreCount) {
-  //     setDisplayResultTexts((prevResultTexts) => {
-  //       const newResultTexts = [...prevResultTexts];
-  //       newResultTexts[index] = "アルバトロス";
-  //       return newResultTexts;
-  //     });
-  //   } else if (parCount - 2 === scoreCount) {
-  //     setDisplayResultTexts((prevResultTexts) => {
-  //       const newResultTexts = [...prevResultTexts];
-  //       newResultTexts[index] = "イーグル";
-  //       return newResultTexts;
-  //     });
-  //   } else if (parCount - 1 === scoreCount) {
-  //     setDisplayResultTexts((prevResultTexts) => {
-  //       const newResultTexts = [...prevResultTexts];
-  //       newResultTexts[index] = "バーディー";
-  //       return newResultTexts;
-  //     });
-  //   } else if (parCount === scoreCount) {
-  //     setDisplayResultTexts((prevResultTexts) => {
-  //       const newResultTexts = [...prevResultTexts];
-  //       newResultTexts[index] = "パー";
-  //       return newResultTexts;
-  //     });
-  //   } else if (parCount + 1 === scoreCount) {
-  //     setDisplayResultTexts((prevResultTexts) => {
-  //       const newResultTexts = [...prevResultTexts];
-  //       newResultTexts[index] = "ボギー";
-  //       return newResultTexts;
-  //     });
-  //   } else if (parCount + 2 === scoreCount) {
-  //     setDisplayResultTexts((prevResultTexts) => {
-  //       const newResultTexts = [...prevResultTexts];
-  //       newResultTexts[index] = "ダブルボギー";
-  //       return newResultTexts;
-  //     });
-  //   }
-  // };
   const parCountsChange = (index: number, value: number) => {
     if (parCounts[index] < 1) return;
     const newParCount = [...parCounts]; // parCounts配列をコピー　新しい配列の作成
     newParCount[index] += value; // 新しい配列のindex番目にvalueを足す
     setParCounts(newParCount);
   };
+
   const scoreCountsChange = (index: number, value: number) => {
     if (scoreCounts[index] < 1) return;
     const newScoreCount = [...scoreCounts];
     newScoreCount[index] += value;
     setScoreCounts(newScoreCount);
   };
+
   const createTableRows = () => {
     const tableRows = [];
     for (let i = 1; i < 19; i++) {
@@ -163,9 +132,10 @@ function App() {
             </div>
           </td>
           <td>
-            <p className="text-sm">
-              {displayResultTexts[i] ? displayResultTexts[i] : ""}
-            </p>
+            <p className="text-sm">{calculateScoreDifference(i)}</p>
+          </td>
+          <td>
+            <p className="text-sm">{displayResultText(i)}</p>
           </td>
         </tr>
       );
@@ -174,13 +144,7 @@ function App() {
   };
   return (
     <div className="App relative">
-      <header className="App-header">
-        <img
-          src="/logo01.png"
-          className="absolute left-8 top-8 w-40"
-          alt="logo"
-        />
-      </header>
+      <Header />
       <div className="box absolute left-1/2 top-1/2 z-10 overflow-y-scroll bg-white p-4">
         <div className="wrap">
           <table className="w-full">
@@ -189,7 +153,8 @@ function App() {
                 <th className="h-12 w-20 text-xl">ホール</th>
                 <th className="h-12 w-32 text-xl">パー</th>
                 <th className="h-12 w-32 text-xl">スコア</th>
-                <th className="h-12 text-xl">コメント</th>
+                <th className="h-12 text-xl">キロク</th>
+                <th className="h-12 text-xl">メモ</th>
               </tr>
             </thead>
             <tbody>{createTableRows()}</tbody>
---
+++ birdies/src/components/header.tsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+  return (
+    <header className="App-header">
+      <img
+        src="/logo01.png"
+        className="absolute left-8 top-8 w-40"
+        alt="logo"
+      />
+    </header>
+  );
+};
